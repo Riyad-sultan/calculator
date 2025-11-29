cfd26910d6ac6ceee521cce2f451abeacf938fec
Riyad-sultan <sultanriyad615@gmail.com>
2025-11-29 16:32:36 +0300

    js basics and advanced(ES6) and React basics

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+// function App() {
+//   return <h1>Hello Riyad, Welcome to React!</h1>;
+// }
+
+// export default App;//makes it usable in other files
+
+//components
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
